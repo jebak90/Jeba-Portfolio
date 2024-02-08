3bfab3bf3c0dd37412a594519e7526adc8ddb64b
Jeba <jpriskillal@gmail.com>
2024-02-08 09:41:32 +0530

    Add Project

--- src/components/projects/ProjectsList.jsx
+++ src/components/projects/ProjectsList.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import kea from '../../assets/kea.png';
 import covid from '../../assets/covid.jpg';
+import SHI from '../../assets/star.jpg';
 import getfocussed from '../../assets/getfocussed.jpg';
 import './projects.css';
 import ProjectButton from "./ProjectButton";
@@ -9,6 +10,7 @@ import Title from "../sectionhead/Title";
 const ProjectsList = ()=>{
 
     const [projectsList, setProjectsList] = useState([
+        {projectName:"Star Health Insurance", projectImage:SHI, description:`SHI has two online applications used by agents and customers to punch health insurance policies for individuals and groups of people.`, languages:['React','Material UI', 'Cypress']},
         {projectName:"Kea", projectImage:kea, description:`A cloud based low-code data platform, that can mine millions of rows of data to provide answers and more, in the language you speak. We would like to call it a Smart, Virtual Data Analyst by your side, always!"`, languages:['React','Tailwind','JavaScript']},
         {projectName:"GetFocused", projectImage:getfocussed, description:`Get Focused is a mobile educational application that lets students track their studies & manage time. It also lets them take breaks & maintain a healthy mind."`, languages:['ReactNative','JavaScript']},
         {projectName:"Covid Risk Assessment", projectImage:covid, description:`The app uses advanced algorithms to analyze user data, including symptoms, exposure history, and travel patterns, to generate a risk score. "`, languages:['Android','Rasa']},
